e96a460b7cde73485f61afc602a35de708773ed9
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2022-12-15 12:58:35 +0100

    first commit

---
+++ TreIRad/TreIRad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TreIRad
+//
+//  Created by Ernesto Carocca on 2022-12-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
